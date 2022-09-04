583057426c7477ab1f17ee08161230201cb2fef6
Juliano Kluge <kluge233@hotmail.com>
2022-09-04 10:35:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Its me Mario!!");
+Console.WriteLine("Chose height a columm: ");
+
+int c = 0;
+int j;
+ c = int.Parse(Console.ReadLine());
+
+
+for( int i = 1; i <= c; i++){
+    
+    for (j = c - i; j>= 1; j-- )
+    Console.Write(" ");
+    for(j = 1; j <= i; j++){
+        Console.Write("#");
+    }        
+    Console.Write(" ");
+    for(j = 1; j <= i; j++){
+        Console.Write("#");
+    }        
+     Console.WriteLine(" ");
+}
